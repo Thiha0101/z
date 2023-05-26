fffb9e06e0c9b03a72222940063e6e54bdb32c39
CryptoGod <133327854+Thiha0101@users.noreply.github.com>
2023-05-26 11:26:48 +0630

    Update ccminer.sh

--- ccminer.sh
+++ ccminer.sh
@@ -1,5 +1,8 @@
-sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential
+sudo apt-get update -y
+sudo apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+sudo apt install git -y
 git clone --single-branch -b ARM https://github.com/monkins1010/ccminer
+sudo apt install nano
 cd ccminer
 chmod +x build.sh
 chmod +x configure.sh
